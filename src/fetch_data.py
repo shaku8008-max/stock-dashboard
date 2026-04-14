cfbb6df8757c6bffcee0dca8f8687077907d28f2
Hamim <shaku8008@gmail.com>
2026-04-14 21:22:22 +1000

    make fetch script safer

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,9 +1,15 @@
+import os
 import yfinance as yf
 import pandas as pd
 
-df = yf.download("AAPL", period="1d", interval="5m")
-df.reset_index(inplace=True)
+os.makedirs("data", exist_ok=True)
 
-df.to_csv("data/test.csv", index=False)
+df = yf.download("AAPL", period="1d", interval="5m", progress=False)
 
-print("Data updated!")
+if df.empty:
+    print("No data returned from yfinance.")
+else:
+    df.reset_index(inplace=True)
+    df.to_csv("data/test.csv", index=False)
+    print("Data updated!")
+    print(df.tail())
